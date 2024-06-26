27c8a559d8f910d2e3aa2908e533cde5cf1496f7
saadiqbal19114 <saadiqbal19114@gmail.com>
2024-06-26 12:21:28 +0500

    Build he upcoming webinar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import MusicSchoolTestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
@@ -11,7 +12,7 @@ export default function Home() {
         <FeaturedCourses/>
         <WhyChooseUs/>
         <MusicSchoolTestimonialCards/>
-        
+        <UpcomingWebinars/>
       </main>
   );
 }
