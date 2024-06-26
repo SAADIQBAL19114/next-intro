0c9e6baf43d26b840bfa8335cf2aee4c26da8cdd
saadiqbal19114 <saadiqbal19114@gmail.com>
2024-06-26 15:15:32 +0500

    Build the footer component

--- src/components/Instructors.tsx
+++ src/components/Instructors.tsx
@@ -36,21 +36,23 @@ const instructors = [
 
 const Instructors = () => {
   return (
-    <div className="relative h-[40rem] overflow-hidden felx items-center justify-center">
-      <WavyBackground className="w-full max-w-7xl mx-auto flex flex-col justify-center items-center h-full">
-        <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8">
-          Meet Our Instructors
-        </h2>
-        <p className="text-base md:text-lg text-white text-center mb-4">
-          Discover the talented professionals who will guide your musical
-          journey
-        </p>
-        <div className="flex flex-row items-center justify-center mb-10 w-full">
-          <AnimatedTooltip items={instructors} />
-        </div>
+    <div className="relative max-h-[40rem] overflow-hidden flex items-center justify-center">
+      <WavyBackground className="w-full max-w-7xl mx-auto flex flex-col items-center justify-center h-full">
+          <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8">
+            Meet Our Instructors
+          </h2>
+          <p className="text-base md:text-lg text-white text-center mb-4">
+            Discover the talented professionals who will guide your musical
+            journey
+          </p>
+          <div className="flex flex-row items-center justify-center mb-10 w-full">
+            <AnimatedTooltip items={instructors} />
+          </div>
+       
       </WavyBackground>
     </div>
   );
 };
+
 
 export default Instructors;
